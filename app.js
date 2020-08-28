428c61e088046d534ab5ad34ad5de812b10bcca3
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-08-29 00:38:02 +0530

    Add mongoDB connection

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+const { getDbConnectionString } = require('./config');
+
 const PORT = process.env.PORT || 3000;
 
 app.use('/assets', express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
+
+mongoose.connect(getDbConnectionString());
 
 app.listen(PORT);
